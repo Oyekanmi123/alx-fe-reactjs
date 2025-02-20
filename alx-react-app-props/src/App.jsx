0dec73dcff9ea48fc156b9f1a72f0185a91ece4d
Lydia Oyekanmi <lydiaebunoluwa8@gmail.com>
2025-02-20 22:05:18 +0100

    Refactored to use Context API

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -8,7 +8,7 @@ import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
 import React from 'react'
-import UserContext from "./UserContext";
+import UserContext from "./context/UserContext";
 import ProfilePage from './ProfilePage'
 
 function App() {
