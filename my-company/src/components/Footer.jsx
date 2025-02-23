c8acadcb9d0755c8e08c3f124e0eac850aae45db
Lydia Oyekanmi <lydiaebunoluwa8@gmail.com>
2025-02-23 20:05:21 +0100

    Initial commit for my-company project

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+      <footer style={{ padding: '10px', background: '#333', color: 'white', textAlign: 'center' }}>
+        <p>&copy; 2024 My Company. All rights reserved.</p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
