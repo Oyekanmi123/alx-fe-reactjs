3e91f200f6525a1465eff56e0912f19568c46586
Lydia Oyekanmi <lydiaebunoluwa8@gmail.com>
2025-02-23 22:36:05 +0100

    Fix errors

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={{ padding: "10px", background: "#f4f4f4" }}>
+    <nav style={{ padding: "10px", backgroundColor: "#333", display: "flex", justifyContent: "space-between"  }}>
       <Link to="/" style={{ marginRight: "10px" }}>Home</Link>
       <Link to="/about" style={{ marginRight: "10px" }}>About</Link>
       <Link to="/services" style={{ marginRight: "10px" }}>Services</Link>
