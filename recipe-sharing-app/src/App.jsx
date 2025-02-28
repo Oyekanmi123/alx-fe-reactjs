cfa19f739ff4f7043d2fe0eaf1445ab20485309b
Lydia Oyekanmi <lydiaebunoluwa8@gmail.com>
2025-03-01 00:42:13 +0100

    checked for routing implementation

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -32,7 +32,7 @@
 //   )
 // }
 
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import RecipeDetails from './components/RecipeDetails';
 import AddRecipeForm from './components/AddRecipeForm';
