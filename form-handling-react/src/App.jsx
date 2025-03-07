6c649bfe6f30f8a0366f7c2c0a7331587d213c2f
Lydia Oyekanmi <lydiaebunoluwa8@gmail.com>
2025-03-07 19:52:11 +0100

    Implement controlled components and Formik form handling

---
+++ form-handling-react/src/App.jsx
@@ -0,0 +1,20 @@
+// import { useState } from 'react'
+import RegistrationForm from './components/RegistrationForm'
+import FormikForm from './components/formikForm'
+
+
+function App() {
+  
+  return (
+    <>
+       <div className="App">
+        <h1>User Registration</h1>
+        <RegistrationForm />
+        <FormikForm />
+      </div>
+      
+    </>
+  )
+}
+
+export default App
